f4ff7520947d625582a022a76cefc94d5c36d577
Jay Looney <jay.m.looney@gmail.com>
2020-06-27 01:25:45 -0500

    Initial CSV Processing Code for RIR Files

--- netblocks.go
+++ netblocks.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"io"
-	"log" // TODO: I need to re-learn how logging in go works.
+	"log" // TODO: I need to learn how logging in go works.
 	"net/http"
 	"net/url"
 	"os"
@@ -15,6 +16,8 @@ import (
 )
 
 // https://github.com/ioerror/blockfinder/blob/master/block_finder/blockfinder.py
+// TODO: Consider using this library for IANA reserved netblocks https://godoc.org/github.com/c-robinson/iplib/iana
+// IPLib is the whole library there, and in-fact the entire thing may be useful.
 
 var COUNTRY_CODE_URL = []string{
 	"http://www.iso.org/iso/home/standards/country_codes/country_names_and_code_elements_txt-temp.htm",
@@ -92,6 +95,8 @@ func ftpDownloadFile(ftp_uri string, target_filepath string) (err error) {
 
 func downloadMaxmindFiles(data_folder string) (err error) {
 	// TODO: Find a new source for this information, this domain seems to be down.
+	// TODO: Consider switching this out for a library
+	// https://godoc.org/?q=maxmind
 	var MAXMIND_URLS = []string{
 		"http://geolite.maxmind.com/download/geoip/database/GeoIPCountryCSV.zip",
 		"http://geolite.maxmind.com/download/geoip/database/GeoIPv6.csv.gz",
@@ -111,6 +116,7 @@ func downloadMaxmindFiles(data_folder string) (err error) {
 }
 
 // https://en.wikipedia.org/wiki/National_Internet_registry
+// IRR - https://www.arin.net/resources/manage/irr/
 func downloadRIRFiles(data_folder string) (err error) {
 	// Consider adding a LongName
 	// Number Resource Organization
@@ -218,14 +224,94 @@ func initializeDatabases() (err error) {
 func queryByCountryCode(country_code string) string {
 	// Use a Sqlite3 or other fast local db
 	// Allow a config file ?
+	// if country_code.Length() > 2: this is invalid.
 
 	return country_code
 }
 
-var country_code string
-var initialize_databases bool
+/*
+struct RIRFile {
+	header RIRHeader
+	delegations []RIRResource
 
-func init() {
+}
+
+struct RIRHeader {
+	version_line RIRVersionLine
+	summary_lines []RIRSummaryLine
+}
+
+struct RIRVersionLine {
+	version string 
+	registry string 
+	serial string 
+	records string 
+	startdate string 
+	enddate string 
+	utcoffset string 
+}
+
+struct RIRSummaryLine {
+	registry string 
+	summary_type string 
+	count string 
+	summary string // Just the text summary
+}
+
+struct RIRResource {
+	registry string
+	cc string 
+	resource_type string // could be 'asn', 'ipv4', or 'ipv6
+	start string // first IPv4 address of range or AS number or ASN number
+	value string // count of hosts for range, doesn't have to represent a CIDR range, count of AS from start value
+	date string 
+	status string 
+	opaque_id string // uniquely identifies an organization (Internet Number Resource Holder)
+
+}
+*/
+
+func processRIRDelegations() (err error) {
+	//https://www.nro.net/wp-content/uploads/nro-extended-stats-readme5.txt
+	// all the "delegated-*" files
+	// File Header - version|registry|serial|records|startdate|enddate|UTCoffset
+	// records - number of records in the file excluding blank lines, summary lines, the version line, and comments
+	// *date - yyyymmdd
+	// UTCoffset +/- UTC distance of local RIR producer
+	// Summary Line - registry|*|type|*|count|summary
+	// Records - registry|cc|type|start|value|date|status|opaque-id[|extensions...]
+	// TODO: Check a prebuilt index
+	default_data_folder := "data/"
+	default_filename := "delegated-arin-extended-latest"
+
+	///open the files
+	openfile, err := os.Open(default_data_folder + default_filename)
+	checkErr("Error opening file", err)
+
+    // TODO: Trying to use the CSVReader results in the wrong number of fields
+    reader := csv.NewReader(openfile)
+    reader.Comma = '|'
+
+	filedata, err := reader.ReadAll()
+	checkErr("Error reading file", err)
+
+	// read the lines of the file
+	for i, value := range filedata {
+		// turn the lines of the file into a better format
+		fmt.Printf("i = %d(%T), value %s(%T)\n", i, i, value, value)
+	}
+	return
+}
+
+func checkErr(msg string, err error) {
+	if err != nil {
+		log.Fatal(msg, err)
+	}
+}
+
+
+func main() {
+	// TODO: Learn exactly what is init() in Go and why I do or don't need it.
 	// I'd like a verbosity flag
 	// I need a cache dir with a sane default
 	// I need a User-Agent for fetching delegation files? FTP UA?
@@ -237,22 +323,50 @@ func init() {
 	// TODO: First Two Flags... --init, -cc
 	// --init downloads all the stuff
 	// --cc actually does a query.
+
+	// TODO: Convert flag variables into some kind of "config struct"
+	var country_code string
+	var initialize_databases bool
+	var list_cc bool
+	var reserved_count bool
+
 	flag.BoolVar(&initialize_databases, "init", false, "Use this flag to initialize the databases")
 	flag.StringVar(&country_code, "cc", "", "Specify the country code to look up")
-}
-
-func main() {
+	flag.BoolVar(&list_cc, "list-cc", false, "Print a list of valid country codes")
+	flag.BoolVar(&reserved_count, "reserved-count", false, "Print a count of ARIN Reservations")
 	flag.Parse()
+
+	// --list-cc
+	// --reserved-count
+	if list_cc {
+		// TODO: Check a prebuilt index
+		//rir_data := processRIRDelegations()
+		processRIRDelegations()
+		//country_codes := []string
+		/*
+		fmt.Printf("Country Code List: \n")
+		for country_code := range rir_data.country_codes {
+			fmt.Printf("\t %v", country_code)
+
+		}
+		*/
+		return
+	}
+
+	if reserved_count {
+		processRIRDelegations()
+		return
+	}
 
 	// We would like a maxmind database, an rir database, an lir database, and a country code database
 	// Check that we have these things and if we don't then get them
-	if initialize_databases == true {
+	if initialize_databases {
 		initializeDatabases()
 		return
 	}
 
 	// If we're not initializing the databases, the only other choice is a query.
-	if initialize_databases == false && country_code == "" {
+	if country_code == "" {
 		fmt.Printf("Either use the --init flag or set a 2 character country code with the -cc flag.\n")
 		return
 	}
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@ This project is a clone of [blockfinder](https://github.com/ioerror/blockfinder/
 which is originally written in python by [ioerror](https://github.com/ioerror)
 
 I've created this project to help myself practice and get more familiar with Golang.
+
+I'm sourcing my 3rd party library decisions to https://github.com/avelino/awesome-go when I need them.
+https://en.wikipedia.org/wiki/List_of_assigned_/8_IPv4_address_blocks
